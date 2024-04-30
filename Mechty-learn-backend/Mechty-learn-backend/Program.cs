638cc3ed08498bf0a55de588b9cce1e4124c6bf5
Veronika Gal <galveron@gmail.com>
2024-04-30 17:28:05 +0200

    add - necessary code to Program

--- Mechty-learn-backend/Mechty-learn-backend/Program.cs
+++ Mechty-learn-backend/Mechty-learn-backend/Program.cs
@@ -1,6 +1,113 @@
+using System.Text.Json.Serialization;
+using Mechty_learn_backend.Data;
+using Mechty_learn_backend.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+var config =
+    new ConfigurationBuilder()
+        .AddUserSecrets<Program>()
+        .Build();
+
 var builder = WebApplication.CreateBuilder(args);
+
+ConfigureSwagger();
+
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>((options) =>
+    options.UseSqlServer(config["ConnectionString"]));
+
+AddCors();
+AddIdentity();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseCors("MyAllowSpecificOrigins");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseHttpsRedirection();
+app.MapControllers();
 
 app.Run();
+
+void ConfigureSwagger()
+{
+    builder.Services.AddSwaggerGen(option =>
+    {
+        option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+        option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            In = ParameterLocation.Header,
+            Description = "Please enter a valid token",
+            Name = "Authorization",
+            Type = SecuritySchemeType.Http,
+            BearerFormat = "JWT",
+            Scheme = "Bearer"
+        });
+        option.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                new string[] { }
+            }
+        });
+    });
+}
+
+void AddCors()
+{
+    builder.Services.AddCors(options =>
+    {
+        options.AddPolicy(name: "MyAllowSpecificOrigins",
+            policy  =>
+            {
+                policy
+                    //.WithOrigins("*") //doesn't work with credentials included
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowCredentials()
+                    .SetIsOriginAllowed(origin =>
+                    {
+                        if (string.IsNullOrWhiteSpace(origin)) return false;
+                        // Only add this to allow testing with localhost, remove this line in production!
+                        if (origin.ToLower().StartsWith("http://localhost")) return true;
+                        // Insert your production domain here.
+                        if (origin.ToLower().StartsWith("https://dev.mydomain.com")) return true;
+                        return false;
+                    });
+            });
+    });
+}
+
+void AddIdentity()
+{
+    builder.Services
+        .AddIdentityCore<Adult>(options =>
+        {
+            options.SignIn.RequireConfirmedAccount = false;
+            options.User.RequireUniqueEmail = true;
+            options.Password.RequireDigit = false;
+            options.Password.RequiredLength = 6;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequireLowercase = false;
+        })
+        .AddEntityFrameworkStores<ApplicationDbContext>();
+    
+}
