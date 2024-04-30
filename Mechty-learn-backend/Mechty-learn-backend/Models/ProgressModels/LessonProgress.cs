6e8fb4b4d94c7f5245c815d39314f8225a7362f6
Veronika Gal <galveron@gmail.com>
2024-04-30 16:59:14 +0200

    add - create LessonProgress model

---
+++ Mechty-learn-backend/Mechty-learn-backend/Models/ProgressModels/LessonProgress.cs
@@ -0,0 +1,10 @@
+using Mechty_learn_backend.Data;
+
+namespace Mechty_learn_backend.Models.EducationalModels.EducationalProcess;
+
+public class LessonProgress
+{
+    public Kid Kid;
+    public int LessonId;
+    public Progress Progress;
+}
