3ba93f4bb1b901cfdc46323e2d745d296ccd678f
Veronika Gal <galveron@gmail.com>
2024-04-30 16:57:59 +0200

    add - create ChapterPage model

---
+++ Mechty-learn-backend/Mechty-learn-backend/Models/EducationalModels/ChapterPageModels/ChapterPage.cs
@@ -0,0 +1,9 @@
+namespace Mechty_learn_backend.Models;
+
+public class ChapterPage
+{
+    public int Id;
+    public ChapterPage3DModel ThreeDModel;
+    public ChapterPageText Text;
+    public ChapterPageSound Sound;
+}
