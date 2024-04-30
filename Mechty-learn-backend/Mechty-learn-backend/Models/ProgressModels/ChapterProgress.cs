21b7bc086338248a61441022c63ef83b2cf78f0b
Veronika Gal <galveron@gmail.com>
2024-04-30 16:59:37 +0200

    add - create ChapterProgress model

---
+++ Mechty-learn-backend/Mechty-learn-backend/Models/ProgressModels/ChapterProgress.cs
@@ -0,0 +1,10 @@
+using Mechty_learn_backend.Data;
+
+namespace Mechty_learn_backend.Models.EducationalModels.EducationalProcess;
+
+public class ChapterProgress
+{
+    public Kid Kid;
+    public int ChapterId;
+    public Progress Progress;
+}
