8fc8ac79b6733c455b731027a853f4a0853cb9fa
Veronika Gal <galveron@gmail.com>
2024-05-03 20:40:29 +0200

    change - add icon to adult model

--- Mechty-learn-backend/Mechty-learn-backend/Models/Adult.cs
+++ Mechty-learn-backend/Mechty-learn-backend/Models/Adult.cs
@@ -1,11 +1,9 @@
-using System.Reflection;
-using Azure.Identity;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 
 namespace Mechty_learn_backend.Models;
 
 public class Adult : IdentityUser
 {
     public ICollection<Kid> Kids { get; init; } = new List<Kid>();
+    public int? Adults3DModelId { get; init; }
 }
