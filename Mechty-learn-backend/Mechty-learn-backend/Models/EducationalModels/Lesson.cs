9cfb55581d48fc6aeccfc3f569dcd829acd9e471
Veronika Gal <galveron@gmail.com>
2024-05-04 22:03:55 +0200

    change - add LessonTitle and LessonDescription in Lesson model

--- Mechty-learn-backend/Mechty-learn-backend/Models/EducationalModels/Lesson.cs
+++ Mechty-learn-backend/Mechty-learn-backend/Models/EducationalModels/Lesson.cs
@@ -1,9 +1,13 @@
+using System.Collections;
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
 
 namespace Mechty_learn_backend.Models;
 
 public class Lesson
 {
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; init; }
-    public List<Chapter> Chapters { get; init; } = new List<Chapter>();
+    public string? LessonTitle { get; init; }
+    public string? LessonDescription { get; init; }
 }
