3ddc1156c80ea45281c244014c2a8a5c4216e37b
Veronika Gal <galveron@gmail.com>
2024-05-04 22:09:50 +0200

    add - create ChapterPageTextRepository and interface

---
+++ Mechty-learn-backend/Mechty-learn-backend/Repositories/EducationRepositories/ChapterPageTextRepository.cs
@@ -0,0 +1,56 @@
+using Mechty_learn_backend.Data;
+using Mechty_learn_backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mechty_learn_backend.Repositories;
+
+public class ChapterPageTextRepository : IChapterPageTextRepository
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public ChapterPageTextRepository(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
+    public async Task<int?> CreateChapterPageText(string chapterPageTextUrl, int chapterPageId)
+    {
+        var chapterPageTextInDb = await _dbContext.ChapterPageTexts.FirstOrDefaultAsync(c => c.ChapterPageId == chapterPageId);
+
+        if (chapterPageTextInDb != null)
+        {
+            return null;
+        }
+        
+        var chapterPage = await _dbContext.ChapterPages.FirstOrDefaultAsync(c => c.Id == chapterPageId);
+        
+        if (chapterPage == null)
+        {
+            throw new Exception("Error in CPTR 01");
+        }
+        
+        var chapterPageText = new ChapterPageText() {ChapterPageId = chapterPageId, ChapterPageTextUrl = chapterPageTextUrl, ChapterPage = chapterPage};
+        await _dbContext.ChapterPageTexts.AddAsync(chapterPageText);
+        await _dbContext.SaveChangesAsync();
+
+        var newChapterPageText = await _dbContext.ChapterPageTexts.FirstOrDefaultAsync(c => c.ChapterPageId == chapterPageId);
+
+        if (newChapterPageText == null)
+        {
+            throw new Exception("Error in CPTR 02");
+        }
+        return newChapterPageText.Id;
+    }
+    
+    public async Task<ChapterPageText> GetChapterPageTextById(int chapterPageTextId)
+    {
+        var chapterPageText = await _dbContext.ChapterPageTexts.FirstOrDefaultAsync(c => c.Id == chapterPageTextId);
+
+        if (chapterPageText == null)
+        {
+            throw new Exception("Error in CPTR 03");
+        }
+
+        return chapterPageText;
+    }
+}
