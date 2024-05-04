c848ea8cca7a3a4ae3cffafd42056ad74604f579
Veronika Gal <galveron@gmail.com>
2024-05-04 22:07:44 +0200

    change - add Kid and Lesson in models

--- Mechty-learn-backend/Mechty-learn-backend/Models/ProgressModels/ChapterProgress.cs
+++ Mechty-learn-backend/Mechty-learn-backend/Models/ProgressModels/ChapterProgress.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations.Schema;
 using Mechty_learn_backend.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,8 +7,11 @@ namespace Mechty_learn_backend.Models.EducationalModels.EducationalProcess;
 [PrimaryKey(nameof(Id), nameof(KidId), nameof(ChapterId))]
 public class ChapterProgress
 {
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; init; }
     public string KidId { get; init; }
+    public Kid Kid { get; init; }
     public int ChapterId { get; init; }
-    public Progress Progress { get; init; }
+    public Chapter Chapter { get; init; }
+    public Progress Progress { get; set; }
 }
--- Mechty-learn-backend/Mechty-learn-backend/Models/ProgressModels/LessonProgress.cs
+++ Mechty-learn-backend/Mechty-learn-backend/Models/ProgressModels/LessonProgress.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations.Schema;
 using Mechty_learn_backend.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,8 +7,11 @@ namespace Mechty_learn_backend.Models.EducationalModels.EducationalProcess;
 [PrimaryKey(nameof(Id),nameof(KidId), nameof(LessonId))]
 public class LessonProgress
 {
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; init; }
+    public Kid Kid { get; init; }
     public string KidId { get; init; }
+    public Lesson Lesson { get; init; }
     public int LessonId { get; init; }
-    public Progress Progress { get; init; }
+    public Progress Progress { get; set; }
 }
