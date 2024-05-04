a353f718feaf324d64e6565322a5ade0b0db24b9
Veronika Gal <galveron@gmail.com>
2024-05-04 22:06:41 +0200

    change - add ChapterId and ChapterPage in models

--- Mechty-learn-backend/Mechty-learn-backend/Models/EducationalModels/ChapterPageModels/ChapterPage3DModel.cs
+++ Mechty-learn-backend/Mechty-learn-backend/Models/EducationalModels/ChapterPageModels/ChapterPage3DModel.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
 
 namespace Mechty_learn_backend.Models;
 
+[PrimaryKey(nameof(ChapterPageId))]
 public class ChapterPage3DModel
 {
-    public int Id { get; set; }
-    public string ChapterPage3DModelUrl { get; set; }
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; init; }
+    public int ChapterPageId { get; init; }
+    public ChapterPage ChapterPage { get; init; }
+    public string ChapterPage3DModelUrl { get; init; }
 }
--- Mechty-learn-backend/Mechty-learn-backend/Models/EducationalModels/ChapterPageModels/ChapterPageSound.cs
+++ Mechty-learn-backend/Mechty-learn-backend/Models/EducationalModels/ChapterPageModels/ChapterPageSound.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
 
 namespace Mechty_learn_backend.Models;
 
+[PrimaryKey(nameof(ChapterPageId))]
 public class ChapterPageSound
 {
-    public int Id { get; set; }
-    public string ChapterPageSoundUrl { get; set; }
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; init; }
+    public int ChapterPageId { get; init; }
+    public ChapterPage ChapterPage { get; init; }
+    public string ChapterPageSoundUrl { get; init; }
 }
