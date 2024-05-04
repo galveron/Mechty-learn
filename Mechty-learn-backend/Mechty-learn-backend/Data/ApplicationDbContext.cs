eb2cf71a863e65ae1affcec2c8e4a60105009584
Veronika Gal <galveron@gmail.com>
2024-05-04 22:13:05 +0200

    feat - add new models in dbContext

--- Mechty-learn-backend/Mechty-learn-backend/Data/ApplicationDbContext.cs
+++ Mechty-learn-backend/Mechty-learn-backend/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 using Mechty_learn_backend.Models;
+using Mechty_learn_backend.Models.EducationalModels.EducationalProcess;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,8 +15,18 @@ public class ApplicationDbContext : IdentityDbContext<Adult>
     public DbSet<Kid> Kids { get; init; }
     public DbSet<Kids3DModel> Kids3DModels { get; init; }
     public DbSet<Adults3DModel> Adults3DModels { get; init; }
-    
-    protected override void OnModelCreating(ModelBuilder builder)
+
+    public DbSet<Lesson> Lessons { get; init; }
+    public DbSet<Chapter> Chapters { get; init; }
+    public DbSet<ChapterPage> ChapterPages { get; init; }
+    public DbSet<ChapterPage3DModel> ChapterPage3DModels { get; init; }
+    public DbSet<ChapterPageSound> ChapterPageSounds { get; init; }
+    public DbSet<ChapterPageText> ChapterPageTexts { get; init; }
+
+    public DbSet<ChapterProgress> ChapterProgresses { get; init; }
+    public DbSet<LessonProgress> LessonProgresses { get; init; }
+
+protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
 
@@ -31,6 +42,52 @@ public class ApplicationDbContext : IdentityDbContext<Adult>
         
         builder.Entity<Adults3DModel>()
             .HasMany(e => e.Adults)
+            .WithOne()
+            .OnDelete(DeleteBehavior.NoAction);
+
+        builder.Entity<Lesson>()
+            .HasMany<Chapter>()
+            .WithOne()
+            .HasForeignKey(e => e.LessonId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Entity<Chapter>()
+            .HasMany<ChapterPage>()
+            .WithOne()
+            .OnDelete(DeleteBehavior.Cascade);
+        
+        builder.Entity<ChapterPageSound>()
+            .HasOne(e => e.ChapterPage)
+            .WithOne()
+            .OnDelete(DeleteBehavior.NoAction);
+        
+        builder.Entity<ChapterPageText>()
+            .HasOne(e => e.ChapterPage)
+            .WithOne()
+            .OnDelete(DeleteBehavior.NoAction);
+        
+        builder.Entity<ChapterPage3DModel>()
+            .HasOne(e => e.ChapterPage)
+            .WithOne()
+            .OnDelete(DeleteBehavior.NoAction);
+        
+        builder.Entity<ChapterProgress>()
+            .HasOne(e => e.Chapter)
+            .WithOne()
+            .OnDelete(DeleteBehavior.NoAction);
+        
+        builder.Entity<ChapterProgress>()
+            .HasOne(e => e.Kid)
+            .WithOne()
+            .OnDelete(DeleteBehavior.NoAction);
+        
+        builder.Entity<LessonProgress>()
+            .HasOne(e => e.Lesson)
+            .WithOne()
+            .OnDelete(DeleteBehavior.NoAction);
+        
+        builder.Entity<LessonProgress>()
+            .HasOne(e => e.Kid)
             .WithOne()
             .OnDelete(DeleteBehavior.NoAction);
     }
