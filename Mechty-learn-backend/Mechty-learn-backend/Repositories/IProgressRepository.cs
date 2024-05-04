12a42050fc8efb982a4a5dc3a1607fe90c67e62f
Veronika Gal <galveron@gmail.com>
2024-05-04 22:11:55 +0200

    add - create ProgressRepository and interface

---
+++ Mechty-learn-backend/Mechty-learn-backend/Repositories/IProgressRepository.cs
@@ -0,0 +1,14 @@
+using Mechty_learn_backend.Data;
+using Mechty_learn_backend.Models.EducationalModels.EducationalProcess;
+
+namespace Mechty_learn_backend.Repositories;
+
+public interface IProgressRepository
+{
+    Task<int?> CreateLessonProgress(string kidId, int lessonId);
+    Task<LessonProgress> GetLessonProgress(string kidId, int lessonId);
+    Task<int?> CreateChapterProgress(string kidId, int chapterId);
+    Task<ChapterProgress> GetChapterProgress(string kidId, int chapterId);
+    Task<LessonProgress> UpdateLessonProgress(string kidId, int lessonId, Progress progress);
+    Task<ChapterProgress> UpdateChapterProgress(string kidId, int chapterId, Progress progress);
+}
