666d6898e6c3961f6a48aeab65b39c992d8f28ba
Veronika Gal <galveron@gmail.com>
2024-05-04 22:09:11 +0200

    add - create ChapterRepository and interface

---
+++ Mechty-learn-backend/Mechty-learn-backend/Repositories/EducationRepositories/IChapterRepository.cs
@@ -0,0 +1,9 @@
+using Mechty_learn_backend.Models;
+
+namespace Mechty_learn_backend.Repositories;
+
+public interface IChapterRepository
+{
+    Task<int?> CreateChapter(string chapterTitle, string chapterDescription, int lessonId);
+    Task<Chapter> GetChapterById(int chapterId);
+}
