60b2485a4c69dcae8394a8f1ade46953a2b01b1e
Veronika Gal <galveron@gmail.com>
2024-05-17 08:40:29 +0200

    feature - create Profile page tsx and scss, add it in App and index

--- Mechty-learn-frontend/src/App.tsx
+++ Mechty-learn-frontend/src/App.tsx
@@ -1,18 +1,75 @@
+import { useEffect, useState } from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './Pages/Home'
-import Layout from './Layout'
+import Profile from './Pages/Profile'
+import Layout, { User } from './Layout'
 import './scss/index.scss'
 
+
+
 function App() {
+  const [user, setUser] = useState<User>();
+  const [userId, setUserId] = useState<string>("");
+
+  async function fetchUser(id: string) {
+    let url = `/api/Adults/GetAdultById?id=${id}`
+    const res = await fetch(url,
+      {
+        method: "GET",
+        credentials: 'include',
+        headers: { 'Content-type': 'application/json' }
+      })
+    if (res.status) {
+      const data = await res.json()
+
+      const newUser: User = {
+        userEmail: data.email,
+        userName: data.userName,
+        userId: id,
+        userProgress: 0
+      }
+
+      return newUser
+    }
+    throw Error("Faild to fetch")
+  }
+
+  useEffect(() => {
+    setUserId("8a297dee-6540-43c4-bc93-ac3a189cc7dd")
+  }, [])
+
+  useEffect(() => {
+    if (userId.length) {
+      fetchUser(userId)
+        .then(user => setUser(user))
+    }
+  }, [userId])
+
   const router = createBrowserRouter([
     {
       path: '/',
-      element: <Layout />,
+      element: <Layout
+        userName={user?.userName}
+        userId={user?.userId}
+        userEmail={user?.userEmail}
+        userProgress={user?.userProgress} />,
       //errorElement: <ErrorPage />,
       children: [
         {
           path: '/',
           element: <Home />
+        },
+        {
+          path: '/home',
+          element: <Home />
+        },
+        {
+          path: '/profile',
+          element: <Profile
+            userName={user?.userName}
+            userId={user?.userId}
+            userEmail={user?.userEmail}
+            userProgress={user?.userProgress} />
         }
       ]
     }
