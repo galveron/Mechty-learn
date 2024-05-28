254546b267944947c5b9bce604db1cce15d45ece
Veronika Gal <galveron@gmail.com>
2024-05-28 10:42:01 +0200

    change - update vite.config.ts and Dockerfile

--- Mechty-learn-frontend/vite.config.ts
+++ Mechty-learn-frontend/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-const backendUrl = process.env.BACKEND_URL ?? '';
+import './src/vite-env';
+
+const backendUrl = import.meta.env.VITE_BACKEND_URL ?? '';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
