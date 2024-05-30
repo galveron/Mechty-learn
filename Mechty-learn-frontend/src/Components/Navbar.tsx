3ef0fd1d88c44ba2fe162aeed738297ae4f511f8
Veronika Gal <galveron@gmail.com>
2024-05-30 10:25:37 +0200

    change - add Profile button to navbar

--- Mechty-learn-frontend/src/Components/Navbar.tsx
+++ Mechty-learn-frontend/src/Components/Navbar.tsx
@@ -4,12 +4,18 @@ export interface UserName {
 
 function Navbar(props: UserName) {
     const userName = props.userName
+
+    function navigate() {
+        window.location.href = '/Profile'
+    }
+
     return (
         <>
             <section className="nav-section">
                 {userName ?
                     <div id="user-name">Hello, {userName}!</div>
                     : <>
+                        <button className="profile-btn" onClick={() => navigate()}>Profile</button>
                         <button className="register-btn">Register</button>
                         <button className="signin-btn">Sign in</button>
                     </>
